e68ad884441205683f8965c725573370a844c92a
preye <omusukup@yahoo.com>
2024-10-22 20:03:19 +0100

    fix(deployment): fix deployment bug

--- src/app/services/Hero.tsx
+++ src/app/services/Hero.tsx
@@ -1,22 +1,22 @@
-import ButtonIcon from "@/components/Global/ButtonIcon";
-import Link from "next/link";
+// import ButtonIcon from "@/components/Global/ButtonIcon";
+// import Link from "next/link";
 
-const Hero = () => {
-  return (
-    <section className="max-container padding-container pt-14 md:pt-20 lg:pt-24">
-      <div className="flex items-center flex-col">
-        <h1 className="text-2xl md:text-5xl lg:text-7xl font-bold text-[#1A1A1A] lg:leading-[81px] capitalize text-center">
-          A Few Services Executed Exceptionally Well
-        </h1>
-        <p className="my-3 md:my-6 w-[202px] md:w-full text-center text-[8px] text-[#323337] md:text-sm lg:text-xl">
-          We turn your ideas into digital solutions that grow your business.
-        </p>
-        <Link href="/contact">
-          <ButtonIcon text="start project" />
-        </Link>
-      </div>
-    </section>
-  );
-};
+// const Hero = () => {
+//   return (
+//     <section className="max-container padding-container pt-14 md:pt-20 lg:pt-24">
+//       <div className="flex items-center flex-col">
+//         <h1 className="text-2xl md:text-5xl lg:text-7xl font-bold text-[#1A1A1A] lg:leading-[81px] capitalize text-center">
+//           A Few Services Executed Exceptionally Well
+//         </h1>
+//         <p className="my-3 md:my-6 w-[202px] md:w-full text-center text-[8px] text-[#323337] md:text-sm lg:text-xl">
+//           We turn your ideas into digital solutions that grow your business.
+//         </p>
+//         <Link href="/contact">
+//           <ButtonIcon text="start project" />
+//         </Link>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Hero;
+// export default Hero;
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,20 +1,20 @@
-import Layout from "@/layout";
-import Hero from "./Hero";
-import Services from "./Services";
-import Clients from "../about/Clients";
-import FAQ from "../about/FAQ";
-import ElevateCard from "@/components/Home/ElevateCard";
+// import Layout from "@/layout";
+// import Hero from "./Hero";
+// import Services from "./Services";
+// import Clients from "../about/Clients";
+// import FAQ from "../about/FAQ";
+// import ElevateCard from "@/components/Home/ElevateCard";
 
-const page = () => {
-  return (
-    <Layout>
-        <Hero />
-        <Services />
-        <Clients />
-        <FAQ />
-        <ElevateCard />
-    </Layout>
-  );
-};
+// const page = () => {
+//   return (
+//     <Layout>
+//         <Hero />
+//         <Services />
+//         <Clients />
+//         <FAQ />
+//         <ElevateCard />
+//     </Layout>
+//   );
+// };
 
-export default page;
+// export default page;
